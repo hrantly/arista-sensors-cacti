2a22b98aab3df1192e9796ff326bc3694ce4d2fd
Ognyan Dimitrov <ogi@spnet.net>
2014-09-24 08:17:35 +0300

    update

--- arista-temp.php
+++ arista-temp.php
@@ -8,5 +8,20 @@ if (!isset($_SERVER["argv"][0]) || isset($_SERVER['REQUEST_METHOD'])  || isset($
 if (defined('STDIN')) {
   $host = $argv[1];
   $community = $argv[2];
+  //sessors table oid
+  $tableOid = '.1.3.6.1.2.1.99.1.1';
+  // // Front-panel temp sensor
+  // $oid ='ARISTA-ENTITY-SENSOR-MIB::aristaEntSensorStatusDescr.100006001';
+  // // Fan controller 1 sensor
+  // $oid ='ARISTA-ENTITY-SENSOR-MIB::aristaEntSensorStatusDescr.100006002';
+  // // Fan controller 2 sensor
+  // $oid ='ARISTA-ENTITY-SENSOR-MIB::aristaEntSensorStatusDescr.100006003';
+  // // Switch chip 1 sensor
+  // $oid ='ARISTA-ENTITY-SENSOR-MIB::aristaEntSensorStatusDescr.100006004';
+  // // VRM 1 temp sensor
+  // $oid ='ARISTA-ENTITY-SENSOR-MIB::aristaEntSensorStatusDescr.100006005';
+  // $oid ='ARISTA-ENTITY-SENSOR-MIB::aristaEntSensorStatusDescr.100711101';
+  
+
 }
 
