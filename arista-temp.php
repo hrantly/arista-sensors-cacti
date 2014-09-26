f75ee03ec01a7f13df7a616420a60bd936d6ff93
Ognyan Dimitrov <ogi@spnet.net>
2014-09-26 21:31:16 +0300

    Commit

--- arista-temp.php
+++ arista-temp.php
@@ -17,9 +17,9 @@ if (defined('STDIN')) {
   $ar = new SNMP(SNMP::VERSION_2C, $host, $community);
   $res1 = $ar->walk($oid, true);
   //$res2 =  $ar->walk($desc,true);
-  print_r($res1);
+  //print_r($res1);
   //print_r($res2);
-  die;
+  //die;
 }
 
 // $final = array();
